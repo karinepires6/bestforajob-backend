1f57e29fb12c34d6c2ce9b1b365f090f515f44c8
Karine Araújo <pires.karine6@gmail.com>
2024-01-12 15:11:10 -0300

    impl. GetCandidatesRepository get candidates from mongo

--- src/models/candidate.ts
+++ src/models/candidate.ts
@@ -1,4 +1,5 @@
 export interface Candidate {
+  id: string;
   name: string;
   skills: string[];
 }
--- src/repositories/get-candidates/mongo-get-candidates.ts
+++ src/repositories/get-candidates/mongo-get-candidates.ts
@@ -1,13 +1,17 @@
 import { IGetCandidatesRepository } from "../../controllers/get-candidates/protocols";
+import { MongoClient } from "../../database/mongo";
 import { Candidate } from "../../models/candidate";
 
 export class MongoGetCandidatesRepository implements IGetCandidatesRepository {
   async getCandidates(): Promise<Candidate[]> {
-    return [
-      {
-        name: "Karine Araujo",
-        skills: ["React", "NodeJS", "Golang"],
-      },
-    ];
+    const candidates = await MongoClient.db
+      .collection<Omit<Candidate, "id">>("candidates")
+      .find({})
+      .toArray();
+
+    return candidates.map(({ _id, ...candidate }) => ({
+      ...candidate,
+      id: _id.toHexString(),
+    }));
   }
 }
